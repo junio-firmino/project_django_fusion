c0b70e30925306a5992302662d5e0a9d177b0696
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2021-08-31 22:21:15 -0300

    Add files via upload

---
+++ core/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.core.mail.message import EmailMessage
+
+
+class Contatoforms(forms.Form):
+    nome = forms.CharField(label='Nome', max_length=100)
+    email = forms.EmailField(label='E-mail', max_length=100)
+    assunto = forms.CharField(label='Assunto', max_length=100)
+    mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea())
+
+    def send_mail(self):
+        nome = self.cleaned_data['nome']
+        email = self.cleaned_data['email']
+        assunto = self.cleaned_data['assunto']
+        mensagem = self.cleaned_data['mensagem']
+
+        content = f'nome: {nome}\nEmail: {email}\nAssunto:{assunto}\nMensagem:{mensagem}'
+
+        mail = EmailMessage(subject=assunto,
+                            body=content,
+                            from_email='junio@fusion.com',
+                            to=['junio@gmail.com'],
+                            headers={'Reply-To': email})
+        mail.send()
